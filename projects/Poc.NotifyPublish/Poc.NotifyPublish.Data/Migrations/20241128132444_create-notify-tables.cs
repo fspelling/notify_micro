6c2c03789729be2e1717cbbe5186c2b5c4c7a638
Fernando Spelling <fernando.spelling@etechss.com.br>
2024-11-28 10:27:20 -0300

    migration project

---
+++ projects/Poc.NotifyPublish/Poc.NotifyPublish.Data/Migrations/20241128132444_create-notify-tables.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Poc.NotifyPublish.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class createnotifytables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WebhookEndpoints",
+                columns: table => new
+                {
+                    ID = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Endpoint = table.Column<string>(type: "nvarchar(250)", maxLength: 250, nullable: false),
+                    Event = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Ativo = table.Column<bool>(type: "bit", nullable: false),
+                    DataCriacao = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WebhookEndpoints", x => x.ID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WebhookEndpoints");
+        }
+    }
+}
